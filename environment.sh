b3c1c0088fb2802c0198b52846a8454f2ec0e79b
SunnyChing <sunny567886@gmail.com>
2018-04-24 16:29:53 +0800

    laser map

--- environment.sh
+++ environment.sh
@@ -5,11 +5,11 @@ source /opt/ros/indigo/setup.bash
 echo "...done."
 
 echo "Setting up PYTHONPATH."
-export PYTHONPATH=/home/ubuntu/duckietown/catkin_ws/src:$PYTHONPATH
+export PYTHONPATH=/home/ubuntu/duckietown_old/catkin_ws/src:$PYTHONPATH
 
 echo "Setup ROS_HOSTNAME."
 export ROS_HOSTNAME=$HOSTNAME.local
-export DUCKIETOWN_ROOT=$HOME/duckietown
+export DUCKIETOWN_ROOT=$HOME/duckietown_old
 
 echo "Building machines file..."
 make -C  $DUCKIETOWN_ROOT
